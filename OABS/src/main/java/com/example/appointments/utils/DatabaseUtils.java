0bd2f4b7a984af6b30255447b69f96136cd851d0
P-Odiix <mahingarodin@gmail.com>
2025-01-04 20:32:51 +0200

    "connected with the db"

---
+++ OABS/src/main/java/com/example/appointments/utils/DatabaseUtils.java
@@ -0,0 +1,17 @@
+package com.example.appointments.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtils {
+    private static final String URL = "jdbc:mysql://localhost:3306/appointments_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
+
