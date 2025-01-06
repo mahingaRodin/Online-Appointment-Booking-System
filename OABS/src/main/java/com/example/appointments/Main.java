84a8e0f3138691078113a0e5ef5d9cf77ae01a54
P-Odiix <mahingarodin@gmail.com>
2025-01-06 13:40:06 +0200

    "used the jakarta.servlet api "

--- OABS/src/main/java/com/example/appointments/Main.java
+++ OABS/src/main/java/com/example/appointments/Main.java
@@ -25,15 +25,16 @@ public class Main {
             // Add the web application
             Context context = tomcat.addWebapp(contextPath, baseDir);
 
-            // Add individual servlets
-            tomcat.addServlet(contextPath, "HealthCheckServlet", (Servlet) new HealthCheckServlet());
-            tomcat.addServlet(contextPath, "UserServlet",(Servlet) new RegisterUserServlet());
-            tomcat.addServlet(contextPath, "AppointmentServlet",(Servlet) new GetAppointmentServlet());
+            // Add individual servlets without unnecessary casting
+            tomcat.addServlet(context, "HealthCheckServlet", String.valueOf(new HealthCheckServlet()));
+            tomcat.addServlet(context, "UserServlet", String.valueOf(new RegisterUserServlet()));
+            tomcat.addServlet(context, "AppointmentServlet", String.valueOf(new GetAppointmentServlet()));
 
             // Map servlets to their respective URL patterns
             context.addServletMappingDecoded("/healthcheck", "HealthCheckServlet");
-            context.addServletMappingDecoded("/user/*" ,  "UserServlet");
-            context.addServletMappingDecoded("/appointments/*" , "AppointmentServlet");
+            context.addServletMappingDecoded("/user/*", "UserServlet");
+            context.addServletMappingDecoded("/appointments/*", "AppointmentServlet");
+
             // Start Tomcat
             tomcat.start();
             System.out.println("Tomcat started on http://localhost:9090" + contextPath);
